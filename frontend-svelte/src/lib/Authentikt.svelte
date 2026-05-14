be83b84aef2fb14a2fa7f61694fe496983a2e4f3
Julius Vincent Babies <julius@familie-babies.de>
2026-05-14 17:16:10 +0200

    correct base URL formatting in Authentikt component and add debug warning

--- frontend-svelte/src/lib/Authentikt.svelte
+++ frontend-svelte/src/lib/Authentikt.svelte
@@ -14,8 +14,17 @@
         children?: Snippet;
     } = $props();
 
+    function correctBaseUrl(baseUrl: string) {
+        if (baseUrl.endsWith("authentikt/")) return baseUrl;
+        if (authentikt_debug) console.warn("The base url is not correctly formatted. It should end with authentikt/. Automatically correcting it, but please update the base url to avoid this warning. This warning is only shown in debug mode.");
+        if (baseUrl.endsWith("/")) return baseUrl + "authentikt/";
+        return baseUrl + "/authentikt/";
+    }
+
+    const correctedBaseUrl = $derived(correctBaseUrl(baseUrl));
+
     const instance = new Authentikt({
-        get baseUrl() { return baseUrl },
+        get baseUrl() { return correctedBaseUrl },
         get authentikt_debug() { return authentikt_debug }
     });
     setAuthentiktContext(instance);
