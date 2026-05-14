1ce0b7a91299b2e2858c9e88bc8a1f6ec505a305
Julius Vincent Babies <julius@familie-babies.de>
2026-05-14 14:30:55 +0200

    add publishing configuration and CI workflows for Authentikt library

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.kotlinx.serialization)
     alias(libs.plugins.dokka)
+    `maven-publish`
 }
 
 repositories {
@@ -25,4 +26,46 @@ kotlin {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("core") {
+            from(components["kotlin"])
+            pom {
+                name.set("authentikt-core")
+                description.set("Kotlin/Ktor multi-step authentication flow library")
+                url.set("https://github.com/Julius-Babies/authentikt")
+                licenses {
+                    license {
+                        name.set("MIT")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("Julius-Babies")
+                        name.set("Julius Babies")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/Julius-Babies/authentikt.git")
+                    developerConnection.set("scm:git:ssh://github.com/Julius-Babies/authentikt.git")
+                    url.set("https://github.com/Julius-Babies/authentikt")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/Julius-Babies/authentikt")
+            credentials {
+                username = project.findProperty("gpr.user") as String?
+                    ?: System.getenv("GITHUB_ACTOR")
+                password = project.findProperty("gpr.key") as String?
+                    ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
